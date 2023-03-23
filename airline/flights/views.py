3d24b8f48826bd5f19c9cfab98c571d092bc45b5
ninabws <nina.bianca06@aluno.ifce.edu.br>
2023-03-23 16:53:17 -0300

    .

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Flight, Airport
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Olá, mundo!')
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all()
+    })
+    
+def flight(request, flight_id):
+    flight = Flight.objects.get(id=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight
+    })
